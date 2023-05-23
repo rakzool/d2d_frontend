2da9dd70a30e709110d37565e0add45cba19968c
rakzool <60981094+rakzool@users.noreply.github.com>
2023-05-23 01:02:03 -0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseRoute = "https://d2d-backend.onrender.com/";
+const baseRoute = "https://d2d-backend.onrender.com";
 
 const userLogin = async (email, password) => {
   const slug = "/api/login";
